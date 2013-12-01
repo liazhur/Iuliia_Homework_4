cd2915689cd6106c028a963fd4d5263ae91a0803
Iuliia <liazhur@gmail.com>
2013-12-01 17:22:27 +0200

    flowerutils class

---
+++ src/task6/FlowerUtilsTest.java
@@ -0,0 +1,24 @@
+package task6;
+
+import org.junit.Test;
+import task4.Flower;
+import task4.FlowerUtils;
+import task4.Main;
+import junit.framework.*;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class FlowerUtilsTest extends Main{
+    @Test
+    public void testJoin() throws Exception {
+        FlowerUtils a = new FlowerUtils();
+
+        Flower[] bouquet1 = {Main.rose, Main.tulip, camomile};
+        Flower [] bouquet2 = {rose, sunflower, camomile};
+        Flower []expResult = Flower[] {rose, tulip,  camomile, rose, sunflower, camomile};
+
+        Flower [] res = a.join(bouquet1, bouquet2);
+        assertEquals(res, expResult);
+    }
+}
