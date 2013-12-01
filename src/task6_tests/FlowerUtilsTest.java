2a5dfbebf4981dc3cc7a129a1ce72af686acc2c3
Iuliia <liazhur@gmail.com>
2013-12-01 17:33:08 +0200

    flowerutils class

--- src/task6_tests/FlowerUtilsTest.java
+++ src/task6_tests/FlowerUtilsTest.java
@@ -1,10 +1,9 @@
-package task6;
+package task6_tests;
 
 import org.junit.Test;
 import task4.Flower;
 import task4.FlowerUtils;
 import task4.Main;
-import junit.framework.*;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -14,7 +13,7 @@ public class FlowerUtilsTest extends Main{
     public void testJoin() throws Exception {
         FlowerUtils a = new FlowerUtils();
 
-        Flower[] bouquet1 = {rose, tulip, camomile};
+        Flower[] bouquet1 = {rose, tulip, camomile};   // where keep that data? why it's unable to get it from Main class?
         Flower [] bouquet2 = {rose, sunflower, camomile};
         Flower []expResult =  {rose, tulip,  camomile, rose, sunflower, camomile};
 
